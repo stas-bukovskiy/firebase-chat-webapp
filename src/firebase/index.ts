dd2de5bb89bf45153f0052c69d958f67835703c2
stas-bukovskiy <stas.bukovskyi@gmail.com>
2024-12-21 15:19:50 +0200

    chore: hide sensitive information

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,13 +3,13 @@ import {getFirestore} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAboaS9NaoGCUsbuZOP34dcuSLh77pZsvc",
-    authDomain: "chat-webapp-6634b.firebaseapp.com",
-    projectId: "chat-webapp-6634b",
-    storageBucket: "chat-webapp-6634b.firebasestorage.app",
-    messagingSenderId: "387272261368",
-    appId: "1:387272261368:web:9dff73600ca241bc6e450d",
-    measurementId: "G-SJHHNCXRGF"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
