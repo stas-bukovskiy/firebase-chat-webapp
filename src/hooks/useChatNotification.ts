7f186f96a34ef5e4e7d6809275448273b0f116d4
stas-bukovskiy <stas.bukovskyi@gmail.com>
2025-02-01 20:20:56 +0200

    feat: add hook for displaying of chat notifications

---
+++ src/hooks/useChatNotification.ts
@@ -0,0 +1,97 @@
+import {h} from 'vue'
+import {useNotification} from 'naive-ui'
+import {useRoute, useRouter} from 'vue-router'
+import Avatar from '@/components/Avatar.vue'
+import {useChatStore} from "@/stores/chats.ts";
+import type {ChatAggregate} from "@/services/entities.ts";
+
+export function useChatNotification() {
+    const notification = useNotification()
+    const router = useRouter()
+    const chatStore = useChatStore()
+    const route = useRoute()
+
+    /**
+     * Shows a notification for a chat message.
+     *
+     * Example structure:
+     * {
+     *   id: 'chat123',
+     *   sender: {
+     *     name: 'John Doe',
+     *     avatar: 'https://example.com/avatar.jpg'
+     *   },
+     *   message: 'Hello, check out this message!'
+     * }
+     * @param params
+     */
+    function showChatNotification(params: {
+        data: {
+            chatId: string,
+        },
+        notification: {
+            title: string,
+            body: string
+        }
+    }) {
+        const chatAgg: ChatAggregate = chatStore.getChatByChatId(params.data.chatId)
+        if (!chatAgg) {
+            console.error('Chat not found:', params.data.chatId)
+            return
+        }
+
+        if (route.path.includes(`/chat/${chatAgg.userChat.id}`)) {
+            console.log('Chat is already open:', chatAgg.userChat.id)
+            return
+        }
+
+        notification.create({
+            duration: 5000,
+            content: () => {
+                return h(
+                    'div',
+                    {
+                        // Style for layout and to indicate it's clickable
+                        style: {
+                            display: 'flex',
+                            alignItems: 'start',
+                            cursor: 'pointer'
+                        },
+                        // Handle click to navigate to the chat page
+                        onClick: async () => {
+                            await router.push(`/chat/${chatAgg.userChat.id}`)
+                        }
+                    },
+                    [
+                        // Avatar component with a small margin
+                        h(Avatar, {
+                            chatAgg: chatAgg,
+                            size: 'small',
+                            class: 'me-3 mt-2'
+                        }),
+                        // A container for sender's name and message text
+                        h(
+                            'div',
+                            {},
+                            [
+                                h('div', {
+                                    style: {
+                                        fontWeight: 'var(--n-title-font-weight)',
+                                        fontSize: 'var(--n-title-font-size)',
+                                    }
+                                }, params.notification.title),
+                                h('div', {
+                                    class: 'mt-2'
+                                }, params.notification.body)
+                            ]
+                        )
+                    ]
+                )
+            }
+        })
+    }
+
+    return {
+        showChatNotification
+    }
+}
