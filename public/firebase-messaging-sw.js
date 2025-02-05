12646ef0f7f8db9b4c51758a31990f4b84ccc7ea
stas-bukovskiy <stas.bukovskyi@gmail.com>
2025-02-05 16:41:03 +0200

    chore: remove sensitive information

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -9,13 +9,13 @@ importScripts('https://www.gstatic.com/firebasejs/11.1.0/firebase-messaging-comp
 
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
 firebase.initializeApp({
-    apiKey: "AIzaSyAboaS9NaoGCUsbuZOP34dcuSLh77pZsvc",
-    authDomain: "chat-webapp-6634b.firebaseapp.com",
-    projectId: "chat-webapp-6634b",
-    storageBucket: "chat-webapp-6634b.firebasestorage.app",
-    messagingSenderId: "387272261368",
-    appId: "1:387272261368:web:f856ed7a25e16c3c6e450d",
-    measurementId: "G-T04XGN1YWG"
+     apiKey: "<API_KEY>",
+     authDomain: "<PROJECT_ID>.firebaseapp.com",
+     projectId: "<PROJECT_ID>",
+     storageBucket: "<PROJECT_ID>.appspot.com",
+     messagingSenderId: "<MESSAGING_SENDER_ID>",
+     appId: "<APP_ID>",
+     measurementId: "<MEASUREMENT_ID>"
 })
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
